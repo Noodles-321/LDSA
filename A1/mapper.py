03e386e438241114e4854fc53090dd28e8bfdb45
Jiahao LU <Jiahao.Lu.2199@student.uu.se>
2019-04-05 19:18:51 +0200

    part2

---
+++ A1/mapper.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import json
+
+Pronouns = ["han", "hon", "den", "det", "denna", "denne", "hen"]
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    
+    if line == '\n':
+        continue
+    
+    line = json.loads(line)
+    
+    if line["retweeted"] != False:
+        continue
+    
+    words = line["text"].split()
+    for word in words:
+        word = word.lower()
+        if word in Pronouns:
+            print('%s\t%s' % (word, 1))
